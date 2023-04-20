d1b52dadb0a4309fa1dfad37bb6c75a05eb09d5a
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-04-20 18:09:45 +0300

    fix: statblock now uses script data instead of raw html

--- src/components/StatisticBlock.vue
+++ src/components/StatisticBlock.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+interface StatisticItem {
+  label: string;
+  description: string;
+}
+
+const statItems: StatisticItem[] = [
+  {
+    label: '$4.8m',
+    description: 'Total Sales',
+  },
+  {
+    label: '24',
+    description: 'Official Addons',
+  },
+  {
+    label: '86',
+    description: 'Total Addons',
+  },
+];
+</script>
+
 <template>
   <section class="bg-gray-900 section-border">
     <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
@@ -16,28 +38,16 @@
 
       <div class="mt-8 sm:mt-12">
         <dl class="grid grid-cols-1 gap-4 sm:grid-cols-3">
-          <div class="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center dark:border-gray-800">
+          <div
+            v-for="statItem in statItems"
+            :key="statItem.label"
+            class="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center dark:border-gray-800"
+          >
             <dt class="order-last text-lg font-medium text-gray-500 dark:text-gray-400">
-              Total Sales
+              {{ statItem.description }}
             </dt>
             <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">
-              $4.8m
-            </dd>
-          </div>
-          <div class="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center dark:border-gray-800">
-            <dt class="order-last text-lg font-medium text-gray-500 dark:text-gray-400">
-              Official Addons
-            </dt>
-            <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">
-              24
-            </dd>
-          </div>
-          <div class="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center dark:border-gray-800">
-            <dt class="order-last text-lg font-medium text-gray-500 dark:text-gray-400">
-              Total Addons
-            </dt>
-            <dd class="text-4xl font-extrabold text-blue-600 md:text-5xl">
-              86
+              {{ statItem.label }}
             </dd>
           </div>
         </dl>
