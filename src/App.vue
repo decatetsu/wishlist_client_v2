64a21fbef8f81df7cd2c548897a323f5614651ce
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-04-21 17:49:36 +0300

    refactor: dedicate landing pages to the '/Landing/'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import HeaderBlock from './components/HeaderBlock.vue';
-import IntroBanner from './components/IntroBanner.vue';
-import StatisticBlock from './components/StatisticBlock.vue';
+import IntroBanner from './components/Landing/IntroBanner.vue';
+import StatisticBlock from './components/Landing/StatisticBlock.vue';
 import FooterBlock from './components/FooterBlock.vue';
-import FeatureBlock from './components/FeatureBlock.vue';
+import FeatureBlock from './components/Landing/FeatureBlock.vue';
 
 </script>
 
