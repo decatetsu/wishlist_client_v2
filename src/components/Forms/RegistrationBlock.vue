72c3de2dae8c92a82df81dfc77e259e44e39259a
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-04-23 19:25:49 +0300

    feat: add validation to registration form

--- src/components/Forms/RegistrationBlock.vue
+++ src/components/Forms/RegistrationBlock.vue
@@ -1,5 +1,42 @@
 <script setup lang="ts">
 import { GiftIcon } from '@heroicons/vue/24/solid';
+import { useForm, useField, useIsFormValid } from 'vee-validate';
+import * as yup from 'yup';
+
+const welcomeHeader = 'Welcome to Wishlist!';
+const welcomeDescription = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vel pariatur deserunt dolor a accusantium tempora hic modi ab explicabo.';
+
+const validationSchema = yup.object({
+  full_name: yup.string().label('Full name').required().min(4).max(40),
+  username: yup.string().label('Username').required().min(5).max(25),
+  email: yup.string().label('Email').required().email().max(70),
+  password: yup.string().label('Password').required().min(8).max(32),
+  password_confirmation: yup.string().label('Password').required()
+    .min(8).max(32).oneOf([yup.ref('password')], 'Passwords must match'),
+});
+
+const { errors, handleSubmit } = useForm({
+  validationSchema,
+  initialValues: {
+    full_name: '',
+    username: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  }
+});
+
+const isValid = useIsFormValid();
+
+const onSubmit = handleSubmit(values => {
+  alert(JSON.stringify(values, null, 2));
+});
+
+const { value: full_name } = useField<string>('full_name');
+const { value: username } = useField<string>('username');
+const { value: email } = useField<string>('email');
+const { value: password } = useField<string>('password');
+const { value: password_confirmation } = useField<string>('password_confirmation');
 </script>
 
 <template>
@@ -7,24 +44,23 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
     <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
       <section class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
         <img
-          class="absolute inset-0 h-full w-full object-cover opacity-80"
-          src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          class="absolute inset-0 h-full w-full object-cover opacity-40"
+          src="https://unsplash.com/photos/Lk3niOsNhdE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mjh8fGdpZnR8ZW58MHx8fHwxNjgyMjAyNjgy&force=true&w=1920"
           alt="Night Image"
         />
 
         <div class="hidden lg:relative lg:block lg:p-12">
           <RouterLink to="/" class="block text-white">
             <span class="sr-only">Home</span>
-            <GiftIcon class="h-8 sm:h-10" />
+            <GiftIcon class="h-8 sm:h-10 text-teal-400" />
           </RouterLink>
 
           <h2 class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-            Welcome to Wishlist!
+            {{ welcomeHeader }}
           </h2>
 
           <p class="mt-4 leading-relaxed text-white/90">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus 
-            vel pariatur deserunt dolor a accusantium tempora hic modi ab explicabo
+            {{ welcomeDescription }}
           </p>
         </div>
       </section>
@@ -44,16 +80,15 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
             </RouterLink>
 
             <h1 class="mt-2 text-2xl font-bold text-gray-900 dark:text-white sm:text-3xl md:text-4xl">
-              Welcome to Wishlist!
+              {{ welcomeHeader }}
             </h1>
 
             <p class="mt-4 leading-relaxed text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus 
-              vel pariatur deserunt dolor a accusantium tempora hic modi ab explicabo
+              {{ welcomeDescription }}
             </p>
           </div>
 
-          <form action="#" class="mt-8 grid grid-cols-6 gap-6">
+          <form @submit="onSubmit" class="mt-8 grid grid-cols-6 gap-6">
             <div class="col-span-6 sm:col-span-3">
               <label
                 for="FullName"
@@ -65,8 +100,11 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
                 type="text"
                 id="FullName"
                 name="full_name"
+                placeholder="Dark Vanholme"
+                v-model="full_name"
                 class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
               />
+              <p class="mt-1 text-red-500 text-xs leading-0">{{ errors.full_name }}</p>
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label
@@ -79,8 +117,11 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
                 type="text"
                 id="Username"
                 name="username"
+                placeholder="darkvanholme"
+                v-model="username"
                 class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
               />
+              <p class="mt-1 text-red-500 text-xs leading-0">{{ errors.username }}</p>
             </div>
             <div class="col-span-6">
               <label
@@ -94,8 +135,11 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
                 type="email"
                 id="Email"
                 name="email"
+                placeholder="user@mail.com"
+                v-model="email"
                 class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
               />
+              <p class="mt-1 text-red-500 text-xs leading-0">{{ errors.email }}</p>
             </div>
 
             <div class="col-span-6 sm:col-span-3">
@@ -110,8 +154,10 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
                 type="password"
                 id="Password"
                 name="password"
+                v-model="password"
                 class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
               />
+              <p class="mt-1 text-red-500 text-xs leading-0">{{ errors.password }}</p>
             </div>
 
             <div class="col-span-6 sm:col-span-3">
@@ -126,24 +172,10 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
                 type="password"
                 id="PasswordConfirmation"
                 name="password_confirmation"
+                v-model="password_confirmation"
                 class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
               />
-            </div>
-
-            <div class="col-span-6">
-              <label for="MarketingAccept" class="flex gap-4">
-                <input
-                  type="checkbox"
-                  id="MarketingAccept"
-                  name="marketing_accept"
-                  class="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:focus:ring-offset-gray-900"
-                />
-
-                <span class="text-sm text-gray-700 dark:text-gray-200">
-                  I want to receive emails about events, product updates and
-                  company announcements.
-                </span>
-              </label>
+              <p class="mt-1 text-red-500 text-xs leading-0">{{ errors.password_confirmation }}</p>
             </div>
 
             <div class="col-span-6">
@@ -161,7 +193,8 @@ import { GiftIcon } from '@heroicons/vue/24/solid';
 
             <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
               <button
-                class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white"
+                :disabled="!isValid"
+                class="inline-block shrink-0 rounded-md border border-teal-600 disabled:border-none bg-teal-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent dark:hover:bg-transparent dark:hover:bg-teal-700 focus:outline-none focus:ring hover:text-teal-500 dark:hover:text-white disabled:bg-gray-500 hover:disabled:bg-gray-500 dark:hover:disabled:bg-gray-500 disabled:cursor-not-allowed"
               >
                 Create an account
               </button>
