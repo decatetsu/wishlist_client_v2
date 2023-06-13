2e47d3c9c2f6255eaea9def48349a5559903c6df
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-06-13 14:09:12 +0300

    fix: remove icon, move username

--- src/components/App/Header/AppHeader.vue
+++ src/components/App/Header/AppHeader.vue
@@ -1,8 +1,6 @@
 <script setup>
-import { GiftIcon } from '@heroicons/vue/24/solid';
 import SearchBar from '@/components/App/Header/SearchBar.vue';
 import UserDropdown from "@/components/App/Header/UserDropdown.vue";
-import { ROUTES } from "@/utils/constants/routes.constants";
 
 const user = {
   username: 'username',
@@ -14,13 +12,6 @@ const user = {
   <header aria-label="Site Header" class="bg-white dark:bg-gray-900 transition ease-linear duration-300">
     <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between gap-8">
-        <div class="md:flex md:items-center md:gap-12">
-          <RouterLink class="block" :to="ROUTES.PROFILE_PATH">
-            <span class="sr-only">Home</span>
-            <GiftIcon class="h-8 text-teal-500"/>
-          </RouterLink>
-        </div>
-
         <div class="hidden md:flex flex-1 items-center justify-end md:justify-between">
           <SearchBar/>
         </div>
--- src/components/App/Header/UserDropdown.vue
+++ src/components/App/Header/UserDropdown.vue
@@ -23,7 +23,7 @@ const menuItems: MenuItem[] = [
   {
     name: 'Logout',
     link: ROUTES.LOGOUT_PATH
-  }
+  },
 ];
 
 function toggleDropdown() {
@@ -39,7 +39,13 @@ function dropdownBlur() {
   <div class="relative">
     <div
       class="inline-flex items-center overflow-hidden bg-white dark:bg-gray-900">
-      <img :src="defaultProfilePicture" alt="avatar" class="w-6 h-6 rounded-full border border-gray-400 dark:border-gray-600">
+      <RouterLink
+        :to="ROUTES.PROFILE_PATH"
+        class="flex text-gray-50 dark:text-gray-300 gap-2 hover:bg-gray-800/60 py-1 px-3 rounded-md"
+      >
+        <span class="text-sm">username</span>
+        <img :src="defaultProfilePicture" alt="avatar" class="w-6 h-6 rounded-full border border-gray-400 dark:border-gray-600">
+      </RouterLink>
 
       <button
         @click="toggleDropdown"
@@ -57,24 +63,16 @@ function dropdownBlur() {
 
     <div
       v-if="dropdownOpen"
-      class="absolute end-0 z-10 mt-2 w-56 rounded-md border border-gray-100 bg-white shadow-lg dark:border-gray-800 dark:bg-gray-900 divide-y divide-gray-100 dark:divide-gray-800"
+      class="absolute end-0 z-10 mt-2 p-2 w-56 rounded-md border border-gray-100 bg-white shadow-lg dark:border-gray-800 dark:bg-gray-900"
     >
-      <div class="p-2">
-        <span class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-gray-500 dark:text-gray-400">
-          Signed as {{username}}
-        </span>
-      </div>
-
-      <div class="p-2">
-        <RouterLink
-          v-for="menuItem in menuItems"
-          :to="menuItem.link"
-          :key="menuItem.name"
-          class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-        >
-          {{ menuItem.name }}
-        </RouterLink>
-      </div>
+      <RouterLink
+        v-for="menuItem in menuItems"
+        :to="menuItem.link"
+        :key="menuItem.name"
+        class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+      >
+        {{ menuItem.name }}
+      </RouterLink>
     </div>
   </div>
 </template>
